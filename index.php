baa1e2626feabe045814858f9c8ed24bfade49b8
mauroluciano <mauroluciano911@gmail.com>
2023-10-02 17:20:42 +0200

    fix

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
-/*
+
 require_once __DIR__ . '/Movie.php';
 require __DIR__ . '/Data.php';
-*/
+
 
 $genere1 = new Genere ('Fantasy');
 $genere2 = new Genere ('Dark');
@@ -19,6 +19,8 @@ $generi = [$genere1, $genere2];
 <!DOCTYPE html>
 <html lang="en">
 <head>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
@@ -32,7 +34,7 @@ $generi = [$genere1, $genere2];
 
         <?php foreach($generi as $genere): ?>
         <div class="row">
-            <?php echo $genere ?>
+            <?php echo $genere->titolo ?>
         </div>
         <?php endforeach; ?>
 </div>
--- movie.php
+++ movie.php
@@ -18,4 +18,22 @@ $this->durata = $durata;
 }
 }
 
+
+class Genere {
+    public $titolo;
+    public $genere;
+    public $durata;
+    
+    
+    public function __construct (
+        string $titolo,
+        string $genere,
+        int $durata,
+    ){
+    $this->titolo = $titolo;
+    $this->genere = $genere;
+    $this->durata = $durata;
+    }
+    }
+
 ?>
